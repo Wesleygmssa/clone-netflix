7fd6e119597836af9c8902f5e553858476bd99f1
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-21 18:55:12 -0300

    update

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { Container, Item, List, ListArea } from "./styles";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 
 export const MovieRow = ({ title, items, slug }) => {
+  const handleLeftArrow = () => {};
+
+  const handleRightArrow = () => {};
   return (
     <Container key={slug}>
       <h2>{title}</h2>
+
       <ListArea>
+        <div className="movireRow-left" onClick={handleLeftArrow}>
+          <NavigateBeforeIcon style={{ fontSize: 50 }} />
+        </div>
+
+        <div className="movireRow-right" nClick={handleRightArrow}>
+          <NavigateNextIcon style={{ fontSize: 50 }} />
+        </div>
         <List className="moview--list">
           {items.results.length > 0 &&
             items.results.map((item, key) => (
