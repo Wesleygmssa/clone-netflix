5e79d16bb6d58f8a87d83f05c41dd0364fb81a74
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-21 11:41:45 -0300

    Criando components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Page } from "./styles";
+import { Tmdb } from "./Tmdb";
 
 export const App = () => {
-  return <div>Teste</div>;
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+      console.log(list);
+    };
+
+    loadAll();
+  }, []);
+  return (
+    <Container>
+      <Page>
+        <Header></Header>
+        <Emphasis></Emphasis>
+        <List></List>
+        <Footer></Footer>
+      </Page>
+    </Container>
+  );
 };
