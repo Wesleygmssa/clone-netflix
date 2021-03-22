1f30116e04af700003773eae893661b804e8f382
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-21 21:35:12 -0300

    verificando erro do link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { Container } from "./styles";
 import User from "../../assets/img/user.png";
-import { Link } from "react-router-dom";
 
 export const Header = ({ black }) => {
   return (
     <Container className={black ? "black" : ""}>
       <div className="logo">
-        <Link to="/">
+        <a href="/">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
             alt="logo"
           />
-        </Link>
+        </a>
       </div>
       <div className="header-user">
-        <Link to="#">
+        <a href="/">
           <img src={User} alt="usuário"></img>
-        </Link>
+        </a>
       </div>
     </Container>
   );
